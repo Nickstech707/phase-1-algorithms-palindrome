c001e32dd5b21392bc630f823129df9532d6f651
Nicholas Muriithi <nickstech707@gmail.com>
2024-04-11 14:33:59 +0300

    Practices algorithmic problem solving

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the correponding letter from the end
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+  return true;
 }
 
 /* 
